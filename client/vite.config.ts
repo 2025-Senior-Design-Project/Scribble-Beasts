a7451592a554a92a5853dfddfaa6ec35c1226f7e
Jasmine-Mogadam <47947909+Jasmine-Mogadam@users.noreply.github.com>
2025-11-18 01:27:54 -0500

    cloudflared :)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,31 +1,33 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import sveltePreprocess from 'svelte-preprocess'
-import path from 'node:path'
-import url from 'node:url'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
+import path from 'node:path';
+import url from 'node:url';
 
-const dirname = path.dirname(url.fileURLToPath(import.meta.url))
+const dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    plugins: [svelte({
-        preprocess: sveltePreprocess(),
-        compilerOptions: {
-            runes: true
-        }
-    })],
-    server: {
-        port: Number(process.env.CLIENT_PORT) || 3000,
-        host: '0.0.0.0',
-        allowedHosts: ['host.docker.internal'],
-        hmr: {
-            protocol: 'ws',
-            host: 'localhost',
-            port: Number(process.env.CLIENT_PORT) || 3000,
-        },
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess(),
+      compilerOptions: {
+        runes: true,
+      },
+    }),
+  ],
+  server: {
+    port: Number(process.env.CLIENT_PORT) || 3000,
+    host: '0.0.0.0',
+    allowedHosts: ['host.docker.internal', 'scribble-beasts.com'],
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: Number(process.env.CLIENT_PORT) || 3000,
     },
-    resolve: {
-        alias: {
-            '@shared': path.resolve(dirname, '../shared'),
-        }
-    }
-})
+  },
+  resolve: {
+    alias: {
+      '@shared': path.resolve(dirname, '../shared'),
+    },
+  },
+});
